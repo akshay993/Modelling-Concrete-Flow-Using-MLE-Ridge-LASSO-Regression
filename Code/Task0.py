4b29c939da5d0cb12b398e89b4c044f50a1ab47c
Akshay993 <akshaytotal1993@gmail.com>
2018-03-21 01:23:53 -0400

    imported data for Task 0

--- Code/Task0.py
+++ Code/Task0.py
@@ -1,2 +1,17 @@
 import glmnet_python
 from glmnet import glmnet
+import numpy as np
+import pandas as pd
+
+#Importing Data from the Slump_Test Data File
+df=pd.read_csv("../Data/slump_test.data")
+
+#Removing the 'No' column from the data frame
+df=df.drop('No',1)
+
+#Randomly Selecting 85 Observations
+for i in range(0,85):
+    
+    
+    
+    
