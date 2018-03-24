872746932b6affd1b2b53ff82c579364b4d322f1
Akshay993 <akshaytotal1993@gmail.com>
2018-03-24 16:54:36 -0400

    Doing 5 fold cross validation

--- Code/Task0.py
+++ Code/Task0.py
@@ -2,6 +2,11 @@ import glmnet_python
 from glmnet import glmnet
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 #Importing Data from the Slump_Test Data File
 df=pd.read_csv("../Data/slump_test.data")
@@ -9,9 +14,24 @@ df=pd.read_csv("../Data/slump_test.data")
 #Removing the 'No' column from the data frame
 df=df.drop('No',1)
 
-#Randomly Selecting 85 Observations
-for i in range(0,85):
+data=df.loc[:,['Cement','Slag','Fly ash','Water','SP','Coarse Aggr.','Fine Aggr.','FLOW(cm)']]
+
+for i in range(0,10):
+        
+    Train, Test=train_test_split(data,test_size=18)
     
-    
-    
-    
+    #Now creating 5 Fold with the 'Train' data
+    kf = KFold(n_splits=5)
+        
+    for train_indices, test_indices in kf.split(Train):
+        train_data = np.array(Train)[train_indices]
+        test_data = np.array(Train)[test_indices]
+        
+        #Converting into numpy array to dataframe
+        traindata=pd.DataFrame(train_data.reshape(68,8),columns=['Cement','Slag','Fly ash','Water','SP','Coarse Aggr.','Fine Aggr.','FLOW(cm)'])
+        testdata=pd.DataFrame(test_data.reshape(17,8),columns=['Cement','Slag','Fly ash','Water','SP','Coarse Aggr.','Fine Aggr.','FLOW(cm)'])
+      
+        xTrain=traindata.loc[:,['Cement','Slag','Fly ash','Water','SP','Coarse Aggr.','Fine Aggr.']]
+        yTrain=traindata.loc[:,['FLOW(cm)']]
+        
+        
