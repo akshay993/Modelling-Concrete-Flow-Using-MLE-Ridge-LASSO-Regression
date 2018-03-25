c1bbb581637f5f0a8f67934b6b5539690d448b3a
Akshay993 <akshaytotal1993@gmail.com>
2018-03-25 18:47:41 -0400

    task2

---
+++ Code/Task2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
+
+from sklearn.model_selection import train_test_split
